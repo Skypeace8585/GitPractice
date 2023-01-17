575962a08f01ab7bb690f9ab14a4b807bac0080f
イオリ <i.takahashi.sys22@morijyobi.ac.jp>
2023-01-17 14:29:30 +0900

    ログイン機能の新規作成

---
+++ src/main/java/servlet/KadaiRegisterConfirmServlet.java
@@ -0,0 +1,68 @@
+package servlet;
+
+import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import dto.KadaiAccount;
+
+/**
+ * Servlet implementation class KadaiRegisterConfirmServlet
+ */
+@WebServlet("/KadaiRegisterConfirmServlet")
+public class KadaiRegisterConfirmServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public KadaiRegisterConfirmServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		request.setCharacterEncoding("UTF-8");
+		
+		String name = request.getParameter("name");
+		String age=request.getParameter("age");
+        int ageInt = Integer.parseInt(age);
+        String gender =request.getParameter("gender");
+        String phone=request.getParameter("phone");
+		String mail = request.getParameter("mail");
+		String pw = request.getParameter("pw");
+		
+		KadaiAccount account = new KadaiAccount(-1,name,ageInt,gender,phone,mail,null,pw,null);
+		
+		// セッションスコープのインスタンス取得
+		HttpSession session = request.getSession();
+		
+		// セッションスコープに値の保存
+		// 第1引数：キー
+		// 第2引数：保存する値
+		session.setAttribute("input_data", account);
+		
+		String view = "WEB-INF/view/Kadai-sample-confirm.jsp";
+		RequestDispatcher dispatcher = request.getRequestDispatcher(view);
+		dispatcher.forward(request, response);
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
